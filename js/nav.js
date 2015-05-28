eba7ab6e65590f1644c445987674cea535266c99
rookieoftheyear <cmaina1000@Collinss-MBP.home>
2015-05-28 14:55:30 -0400

    continued work

---
+++ js/nav.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+
+
+
+jQuery(document).ready(function(){
+	jQuery(".menu-trigger").click(function(){
+		jQuery(".nav-menu").slideToggle(400, function(){
+			JQuery(this).toggleClass("nav-expanded").css('display','');
+			});
+		});
+});
+
+/*
+(function () {
+    
+    // Create mobile element
+    var mobile = document.createElement('div');
+    mobile.className = 'nav-mobile';
+    document.querySelector('.nav').appendChild(mobile);
+
+    // hasClass
+    function hasClass(elem, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
+
+    // toggleClass
+    function toggleClass(elem, className) {
+        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+        if (hasClass(elem, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                newClass = newClass.replace(' ' + className + ' ', ' ');
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        } else {
+            elem.className += ' ' + className;
+        }
+    }
+
+    // Mobile nav function
+    var mobileNav = document.querySelector('.nav-mobile');
+    var toggle = document.querySelector('.nav-list');
+    mobileNav.onclick = function () {
+        toggleClass(this, 'nav-mobile-open');
+        toggleClass(toggle, 'nav-active');
+    };
+})();*/
